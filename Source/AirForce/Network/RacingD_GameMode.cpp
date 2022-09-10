163cda22660b1b4d78d67df0eacd5dea6e5ace51
IkemuraRyota <88326669+IkemuraRyota@users.noreply.github.com>
2022-09-10 12:44:04 +0900

    Gamemode追加

---
+++ Source/AirForce/Network/RacingD_GameMode.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RacingD_GameMode.h"
+
+ARacingD_GameMode::ARacingD_GameMode()
+{
+
+}
---
+++ Source/AirForce/Network/RacingD_GameMode.h
@@ -0,0 +1,26 @@
+﻿/*
+* マルチ対戦時のサーバーで制御する処理をまとめたゲームモードクラス
+*
+*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "RacingD_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRFORCE_API ARacingD_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+    /**
+     * @brief コンストラクタ
+    */
+    ARacingD_GameMode();
+
+};
